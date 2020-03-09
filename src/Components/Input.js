404bead17061e03cb6e4eef806ddaa1068b1af3b
Timothy Barna <audioclimb@gmail.com>
2020-03-09 10:40:36 -0600

    working

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -4,17 +4,47 @@ class Input extends Component {
     constructor(){
         super()
         this.state = {
-
+            name: '',
+            birthMonth: '',
+            favoriteColor: ''
         }
     }
+
+    handleNameChange = (firstName) => {
+        this.setState({
+            name: firstName
+        })
+    }
+
+    handleBirthChange = (event) => {
+        this.setState({
+            birthMonth: event.target.value
+        })
+    }
+
+
 
     render(){
         return(
             <div className='input-boxes'>
                 <p>Name:</p>
-                <input />
+                <input onChange={(e) => this.handleNameChange(e.target.value)}/>
                 <p>Birth Month:</p>
-                <input />
+                {/* <input list="months" type="text" onChange={(e) => this.handleBirthChange(e.target.value)}/> */}
+                <select value={this.state.birthMonth} onChange={this.handleBirthChange} id="months">
+                    <option value="january">January</option>
+                    <option value="february">February</option>
+                    <option value="march">March</option>
+                    <option value="april">April</option>
+                    <option value="may">May</option>
+                    <option value="june">June</option>
+                    <option value="july">July</option>
+                    <option value="august">August</option>
+                    <option value="september">September</option>
+                    <option value="october">October</option>
+                    <option value="november">November</option>
+                    <option value="december">December</option>
+                </select>
                 <p>Favorite Color</p>
                 <input />
                 <button className="create-button">Create</button>
